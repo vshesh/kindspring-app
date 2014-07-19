328a8ed67260da64a1e8d544947f22c66b470df8
Vishesh Gupta <vishesh@stanford.edu>
2014-07-18 18:15:44 -0700

    finished the bootstrapping process

--- server.js
+++ server.js
@@ -24,11 +24,7 @@ var findIndexByAttr = function(array, attr, value) {
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var handlebars = require('express3-handlebars');
-var partials = require('express-partials');
 var fs = require('fs');
-
-var data = require('./data.json');
 
 // //This code lets us test on local, or use heroku properly.
 // var settings;
@@ -42,21 +38,16 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'public'));
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.engine('html', handlebars());
+app.set('views', path.join(__dirname, 'www', 'templates'));
+
 app.use(express.cookieParser());
-app.use(express.session({secret: 'pink unicornians'}));
-//app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.session());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.multipart());
 app.use(express.methodOverride());
-app.use(express.session());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'www')));
 
 // development only
 if ('development' == app.get('env')) {
@@ -64,26 +55,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', function(req, res){ res.render('index.html')});
-app.get('/plans/:name', function(req, res) {
-  var plan = findByAttr(data.plans, "name", req.params.name);
-  if (plan == -1) { res.send(404);}
-  res.json(plan);
-});
-
-app.post('/plans/:name', function(req, res) {
-  var path = req.params.path;
-  var tosave = req.body;
-  var plan = findByAttr(data.plans, "name", req.params.name);
-  if (plan == -1) {
-    data.plans.append(tosave);
-  } else {
-    plan.title = tosave.title;
-    plan.name = tosave.name;
-    plan.features = tosave.features;
-  }
-
-  res.send(200);
-});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
