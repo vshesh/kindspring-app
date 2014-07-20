90c203dc7c01d06cd894598748866bd30bfc2e17
Vishesh Gupta <vishesh@stanford.edu>
2014-07-19 20:23:12 -0700

    basic nagivational flow donw

--- www/js/app.js
+++ www/js/app.js
@@ -4,69 +4,28 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('kindspring-app', ['ngRoute', 'kindspring-app.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "templates/menu.html",
-      controller: 'AppCtrl'
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/main.html',
+      controller: 'MainCtrl'
     })
-
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
-        }
-      }
+    .when('/login', {
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
     })
-
-    .state('app.browse', {
-      url: "/browse",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
+    .when('/home', {
+      templateUrl: 'templates/homepage.html',
+      controller: 'HomepageCtrl'
     })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
+    .when('/post', {
+      templateUrl: 'templates/post.html',
+      controller: 'PostCtrl'
     })
-
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
+    .otherwise({
+      redirectTo: '/'
     });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
 });
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,79 @@
-angular.module('starter.controllers', [])
-
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  },
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+angular.module('kindspring-app.controllers', [])
+.controller('PostCtrl', function($scope, $location) {
+  
 })
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+.controller('HomepageCtrl', function($scope, $location) {
+  $scope.stories = [
+  {
+    title: 'Roses',
+    text: 'roses are red \nviolets are blue \n... fill in the blanks your own way'
+  },
+  {
+    title: 'Haiku',
+    text: 'Haikus are awesome \nBut they can sometimes be strange \nRefrigerator'
+  }
   ];
 })
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('LoginCtrl', function($scope, $location) {
+  $scope.login = function() {
+    $location.path('/home');
+  }
 })
+.controller('MainCtrl',function($scope, $location) {
+  $scope.loginClicked = function() {
+    $location.path('/login');
+  }
+  $scope.stories = [
+  {
+    title: 'Marks of Kindness',
+    text : "I was on my way to my parents' on my motorcycle and saw a middle" + 
+     "aged man carrying a heavy sack on his shoulders and walking with great" + 
+     " struggle. I passed him, then paused, and turned around to stop near him" +
+     " and offer him a ride. He lifted the heavy sack which had a lot of scrap iron"
+  },
+  {
+    title: 'A Kindness That Has Become A Habit',
+    text : "I was on my way to my parents' on my motorcycle and saw a middle" + 
+     "aged man carrying a heavy sack on his shoulders and walking with great" + 
+     " struggle. I passed him, then paused, and turned around to stop near him" +
+     " and offer him a ride. He lifted the heavy sack which had a lot of scrap iron" +
+     " and sat on the pillion. Immediately he asked me my name and after I told him," +
+     " blessed me aloud. We rode on. In five minutes, we were stopped by a traffic" +
+     " cop who demanded to see my license. There were many cops who were stopping" +
+     " everyone on that road. My ride got down with his sack and got back on after I" +
+     " was through with the cop. We reached his destination. He got off and thanked me" +
+     " again. After reaching home, while parking my motorcycle I saw that the iron pieces" +
+     " in the sack that he had placed on the pillion had made deep marks and almost torn the" +
+     " seat in a couple of places. I smiled and took it as a reminder he had left behind" +
+     " so that I do not forget what the pillion is for"    
+  },
+  {
+    title: 'Marks of Kindness',
+    text : "I was on my way to my parents' on my motorcycle and saw a middle" + 
+     "aged man carrying a heavy sack on his shoulders and walking with great" + 
+     " struggle. I passed him, then paused, and turned around to stop near him" +
+     " and offer him a ride. He lifted the heavy sack which had a lot of scrap iron"
+  },
+  {
+    title: 'Marks of Kindness',
+    text : "I was on my way to my parents' on my motorcycle and saw a middle" + 
+     "aged man carrying a heavy sack on his shoulders and walking with great" + 
+     " struggle. I passed him, then paused, and turned around to stop near him" +
+     " and offer him a ride. He lifted the heavy sack which had a lot of scrap iron"
+  },
+  {
+    title: 'Marks of Kindness',
+    text : "I was on my way to my parents' on my motorcycle and saw a middle" + 
+     "aged man carrying a heavy sack on his shoulders and walking with great" + 
+     " struggle. I passed him, then paused, and turned around to stop near him" +
+     " and offer him a ride. He lifted the heavy sack which had a lot of scrap iron"
+  },
+  {
+    title: 'Marks of Kindness',
+    text : "I was on my way to my parents' on my motorcycle and saw a middle" + 
+     "aged man carrying a heavy sack on his shoulders and walking with great" + 
+     " struggle. I passed him, then paused, and turned around to stop near him" +
+     " and offer him a ride. He lifted the heavy sack which had a lot of scrap iron"
+  },
+  ];
+});
